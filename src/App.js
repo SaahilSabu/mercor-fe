d66ff04bb927a52ce8b607c2da8d5bad1c1988d8
SAAHIL SABU HAMEED <saahil.sabu@gmail.com>
2023-08-11 13:17:46 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import React, { useState, useEffect } from "react";
 import Floater from "./components/Floater";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Mobile from "./components/Mobile";
 import Nav from "./components/Nav";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div class="cont overflow-y-hidden">
-      <Floater />
-      <Nav />
-      <Hero />
-      <Footer />
+    <div className="">
+      {isMobile ? (
+        <div className="cont-m bg-black">
+          <Mobile />
+        </div>
+      ) : (
+        <div className="cont overflow-y-hidden">
+          <Floater />
+          <Nav />
+          <Hero />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
